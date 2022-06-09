9dba021e3cba48aed11167d1a85111d10f64e3e6
shaharyar saleem <shaharyarsaleem71@gmail.com>
2022-06-09 14:56:35 +0200

    feat: data fetched using graphQl

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
-export default function userProfile({data}) {
+const UserProfile = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allIndexJson {
+        edges {
+          node {
+            username
+            email
+            createdAt
+            profile {
+              dob
+              name
+              about
+              address
+              company
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log("data fetched:", data)
   return (
     <div>
       <h1>All user profiles</h1>
-      {data}
+      {data.allIndexJson.edges.map((user, key) => {
+        return (
+          <div key={key}>
+            {console.log("inside data:", user.node)}
+            <h4>{user.node.profile.about}</h4>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
-// export const data = useStaticQuery(graphql`
-// query{
-//   allIndexJson {
-//     edges {
-//       node {
-//         username
-//         email
-//         createdAt
-//         profile {
-//           dob
-//           name
-//           about
-//           address
-//           company
-//         }
-//       }
-//     }
-//   }
-// }
-// `)
-
-
+export default UserProfile
